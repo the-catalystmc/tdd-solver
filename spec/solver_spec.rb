b888baf755ede9b6fbabe1d37699937e17abccaa
Rhyine <rhyinestewart@gmail.com>
2021-10-14 14:38:49 -0500

    Create test for reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,9 +2,16 @@ require_relative '../solver'
 
 describe Solver do
   context 'With valid input' do
-    it 'should instansiate a person with age 20' do
+    it 'should return factorial of given number' do
       solver = Solver.new
       expect(solver.factorial(5) == 120).to be true
+    end
+  end
+
+  context 'With valid input' do
+    it 'should reverse given string' do
+      solver = Solver.new
+      expect(solver.reverse('hello') == 'olleh').to be true
     end
   end
 end
