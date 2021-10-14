087335f757c031f279961c4d7d1840b153759925
Gabriel <gabriel.gabrielyea@gmail.com>
2021-10-14 14:25:04 -0500

    Create Factorial test

---
+++ spec/solver_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../solver'
+
+describe Factorial do
+  context 'With valid input' do
+    it 'should instansiate a person with age 20' do
+      solver = Solver.new
+      expect(solver.factorial(5) == 120).to be true
+    end
+  end
+end
