ccbf633c47f8d82c3e229ad5bfaca81d629678a0
Rhyine <rhyinestewart@gmail.com>
2021-10-14 15:18:24 -0500

    Add fizzbuzz tests

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -27,5 +27,24 @@ describe Solver do
         expect(solver.reverse('hello') == 'olleh').to be true
       end
     end
+
+    context 'With valid input' do
+      it 'returns fizz' do
+        solver = Solver.new
+        expect(solver.fizzbuzz(6)).to eq('fizz')
+      end
+      it 'returns buzz' do
+        solver = Solver.new
+        expect(solver.fizzbuzz(5)).to eq('buzz')
+      end
+      it 'returns fizzbuzz' do
+        solver = Solver.new
+        expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+      end
+      it 'returns the correct string' do
+        solver = Solver.new
+        expect(solver.fizzbuzz(11)).to eq('11')
+      end
+    end
   end
 end
